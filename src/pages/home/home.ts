0312f821c00d35e03dc3bb384f0c7fa451cace23
Maurizio Pietrantuono <maurizio.pietrantuon@Mac2210.local>
2018-02-06 12:13:27 +0000

    Done

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  todo = null
+  itemsArray = []
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  add() {
+    if (this.todo != null) {
+      this.itemsArray.push(this.todo)
+      this.todo = null
+    }
+  }
+
+  delete(item: String) {
+    if (item != null) {
+      var index = this.itemsArray.indexOf(item);
+      if (index > -1) {
+        this.itemsArray.splice(index, 1)
+      }
+    }
+  }
 }
